2d325252a0ac6c96321c33762a61369718c16459
robinjmartis <robinjmartis@gmail.com>
2018-09-15 08:11:00 +0530

    adding third program by GitDemo1 user

--- src/test/ThirdProgram.java
+++ src/test/ThirdProgram.java
@@ -1,16 +1,13 @@
 package test;
 
-public class SecondProgram {
+public class ThirdProgram {
 	public static void main(String[] args) {
+		
 		System.out.println("Sample program 1");
 		System.out.println("Sample program 2");
 		System.out.println("Sample program 3");
 		System.out.println("Sample program 4");
-		System.out.println("Sample program 5");
 		
-		System.out.println("Sample program 6");
-		System.out.println("Sample program 7");
-		System.out.println("Sample program 8");
 	}
 
 }
